e0ac96a209de6135089cffa8e0ec31f8862d40af
mk@Shiva <gangwar3537@gmail.com>
2022-09-12 14:57:37 +0530

    added README.MD

--- config/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose')
-//mongoose.connect('mongodb://localhost/astro');
-mongoose.connect(process.env.DB_URI);
-const db=mongoose.connection;
-db.on('error',console.error.bind(console,"Error connecting to MongoDB"));
-db.once('open',function(){
-    console.log('Connected to Database:: MongoDB');
-});
-module.exports=db;
--- index.js
+++ index.js
@@ -9,10 +9,10 @@ app.use(express.static('assets'));
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 app.use('/',require('./route'))
-app.listen(port ,(err)=>{
+app.listen(process.env.PORT || port,(err)=>{
     if(err){
         console.log(`Error in running the server: ${err}`);
         return;
     }
-    console.log(`Server is running on port ${port}`);
+    console.log("Server is running...");
 });
